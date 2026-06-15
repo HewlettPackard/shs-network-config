204eff4efebfae6fd960f43ef0dc90af2d337001
terrence.mitchem@hpe.com <terrence.mitchem@hpe.com>
2026-06-15 10:21:56 -0500

    build: Update build.sh to use the common build.sh in ce-devops (#161)

--- build.sh
+++ build.sh
@@ -1,32 +1,16 @@
 #!/usr/bin/env bash
 #
-# Copyright 2024 Hewlett Packard Enterprise Development LP. All rights reserved.
+# Copyright 2026 Hewlett Packard Enterprise Development LP. All rights reserved.
 #
 
-set -Eeuo pipefail
+set -Eeuox pipefail
 
-CE_BUILD_SCRIPT_REPO=hpc-shs-ce-devops
-
-# Use appropriate ce-devops branch
-if [[ -n "${BRANCH_NAME:-}" && "$BRANCH_NAME" =~ "release/shs-" ]]; then
-    CE_CONFIG_BRANCH=$BRANCH_NAME
-elif [[ -n "${CHANGE_TARGET:-}" && "$CHANGE_TARGET" =~ "release/shs-" ]]; then
-    CE_CONFIG_BRANCH=$CHANGE_TARGET
-else
-    CE_CONFIG_BRANCH=${CE_CONFIG_BRANCH:-main}
-fi
+export CE_BUILD_SCRIPT_REPO=hpc-shs-ce-devops
 
 if [ -d ${CE_BUILD_SCRIPT_REPO} ]; then
-    git -C ${CE_BUILD_SCRIPT_REPO} fetch
-    git -C ${CE_BUILD_SCRIPT_REPO} checkout ${CE_CONFIG_BRANCH}
-    git -C ${CE_BUILD_SCRIPT_REPO} pull
+    echo "Using existing '${CE_BUILD_SCRIPT_REPO}' directory"
 else
-    git clone --branch "${CE_CONFIG_BRANCH}" https://$HPE_GITHUB_TOKEN@github.hpe.com/hpe/${CE_BUILD_SCRIPT_REPO}.git
+    git clone https://$HPE_GITHUB_TOKEN@github.hpe.com/hpe/${CE_BUILD_SCRIPT_REPO}.git
 fi
 
-. ${CE_BUILD_SCRIPT_REPO}/build/sh/rpmbuild/load.sh
-
-setup_dst_env
-
-dst_build_rpm -c ${CE_BUILD_SCRIPT_REPO}/build/configs/network-config.yaml $@
-dst_build_deb --yamlfile ${CE_BUILD_SCRIPT_REPO}/build/configs/${CE_CONFIG_FILE} --ps ${PRODUCT} --main-branch "main" $@
+. ${CE_BUILD_SCRIPT_REPO}/build/sh/rpmbuild/build-common.sh
